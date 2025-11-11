50dac93829031f48bf152652abe921ab834ba8e5
Tatiana-Juin <username@users.noreply.github.com>
2025-11-11 10:57:49 +0100

    J'ai fini le projet il faut juste que j'ameliore le design

---
+++ script.js
@@ -0,0 +1,34 @@
+let textarea = document.getElementById("word-textarea");
+// let nbAllWord = document.querySelector(".nb-all-word");
+let nbWord = document.querySelector(".nb-word");
+let nbCaractere = document.querySelector(".nb-caractere");
+let nbSpace = document.querySelector(".nb-space");
+let btnValider = document.querySelector(".btn-valider")
+let btnReset = document.querySelector(".btn-reset");
+
+btnValider.addEventListener("click", () =>{
+    let text = textarea.value.trim();
+
+    // POUR RECUPERER TOUT LES MOT SANS ESPACE 
+    let nbWordWithoutSpace = text.match(/\S+/g).length;
+    nbWord.textContent=`${nbWordWithoutSpace } mot sans espace`;
+
+    // POUR RECUPERER TOUS LES CARACTERE Y COMPRIS LES ESPACE
+    let nbCaracter = text.length;
+    nbCaractere.textContent=`${nbCaracter} caractere avec espace`;
+
+    // POUR COMPTER LE NB D'ESPACE 
+    let space = text.match(/ /g);
+    
+    nbSpace.textContent=`${space ? space.length : 0 } espace `;
+
+})
+// RESET 
+btnReset.addEventListener("click",() =>{
+    textarea.value ="";
+    nbWord.textContent=" 0 mot sans espace";
+    nbCaractere.textContent="0 caractere avec espace";
+    nbSpace.textContent="0 espace";
+    
+
+})
